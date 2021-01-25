bdecd6db642dae21970650e5da387ed8ddd8dbbc
andybyers21 <andy.byers21@gmail.com>
2021-01-25 16:25:51 +0000

    add godotenv

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_news_app_demo
+
+go 1.15
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,10 +14,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "1313"
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
 	}
+
+	port := os.Getenv("PORT")
+	// NOTE: I don"t think this is required by godotenv but will leave it here for the timebeing for troubleshooting."
+	//	if port == "" {
+	//		port = "1313"
+	//	}
 
 	mux := http.NewServeMux()
 
