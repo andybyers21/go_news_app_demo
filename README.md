77268eeec98b6814113f33f1a31611a4fc81d801
andybyers21 <andy.byers21@gmail.com>
2021-02-02 16:20:39 +0000

    add search handler

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# News App made with Go.
+# News App made with Go
+
+Using [NewsAPI.org](https://newsapi.org/) JSON API.
+
+Create a `.env` file with the following,
+
+```txt
+PORT=
+
+NEWS_API_KEY=
+
+```
+
--- main.go
+++ main.go
@@ -3,18 +3,37 @@ package main
 import (
 	// "fmt"
 	// html/template generate HTML output that is safe against code injection
+	"fmt"
+	"github.com/joho/godotenv"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
 }
 
 func main() {
@@ -36,8 +55,7 @@ func main() {
 
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	// no longer required as running fresh to auto build the server.
-	// fmt.Println("runnung on port :1313")
+	fmt.Println("runnung on port :1313")
 
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
