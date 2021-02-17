76c9bc1845d5e7df563e2383c4b6aa3dc9e3b613
andybyers21 <andy.byers21@gmail.com>
2021-02-17 09:14:52 +0000

    make API requests

--- main.go
+++ main.go
@@ -39,8 +39,14 @@ func searchHandler(newsapi *news.Client) http.HandlerFunc {
 			page = "1"
 		}
 
-		fmt.Println("Search Query is: ", searchQuery)
-		fmt.Println("Page is: ", page)
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println("**** Search Query is: ", searchQuery, "Page is: ", page, "****")
+		fmt.Printf("%+v", results)
 	}
 }
 
