0984fbb2835c0664a77e0c36b0f5a1cb59d079ba
andybyers21 <andy.byers21@gmail.com>
2021-02-18 13:34:21 +0000

    refactor news

--- news/news.go
+++ news/news.go
@@ -64,7 +64,7 @@ func (c *Client) FetchEverything(query, page string) (*Results, error) {
 }
 
 // Create and return a new Client instance for making requests to the News api
-// and return max 100 pages
+// Return max 100 pages
 func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
 	if pageSize > 100 {
 		pageSize = 100
